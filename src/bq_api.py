6895b2b795f1c4295c1306ac787fcdd1e247f395
giulia <csbnex24@gmail.com>
2024-04-24 16:01:21 +0200

    add bq and gcs api scripts

---
+++ src/bq_api.py
@@ -0,0 +1,29 @@
+from google.cloud import bigquery
+from google.api_core.exceptions import NotFound
+import os
+
+class BigQueryApi:
+
+    def __init__(self):
+        self.client = bigquery.Client()
+
+    def exists(self, table_id):
+        try:
+            self.client.get_table(table_id)
+            return True
+        except NotFound:
+            return None
+
+
+
+if __name__ == '__main__':
+    credentials_path = 'pythongcp_privateKey.json'
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+
+    bq_api = BigQueryApi()
+    table_id = "training-gcp-309207.dataset_benevento.scontrini"
+    res = bq_api.exists(table_id)
+    if res:
+        print(f"La tabella {table_id} esiste")
+    else:
+        print(f"La tabella {table_id} non esiste")
---
+++ src/gcs_api.py
@@ -0,0 +1,31 @@
+from google.cloud import storage
+import os
+
+class GcsApi:
+
+    def __init__(self):
+        # Inizializza il client di Google Cloud Storage
+        self.client = storage.Client()
+
+    def check_file_exists(self, bucket_name, file_name):
+
+        # Ottieni il riferimento al bucket
+        bucket = self.client.bucket(bucket_name)
+
+        # Verifica se il blob (file) esiste
+        blob = bucket.blob(file_name)
+        if blob.exists():
+            print(f"Il file {file_name} esiste nel bucket {bucket_name}.")
+        else:
+            print(f"Il file {file_name} non esiste nel bucket {bucket_name}.")
+
+
+
+if __name__ == '__main__':
+    credentials_path = 'pythongcp_privateKey.json'
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+
+    gcs_api = GcsApi()
+    bucket_name = 'bucket_vitali'
+    file_name = 'scontrini.csv'
+    gcs_api.check_file_exists(bucket_name, file_name)
